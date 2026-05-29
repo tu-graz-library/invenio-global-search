8f14c77a80a84f0264de3975b63e29dfc328c987
Eduard Nitu <edi.nitu2000@yahoo.com>
2026-05-29 12:20:31 +0200

    feat(cli): option to create/update only missing records
     * some records from any data model can be missing from
       global-search database for whatever reason. this commit
       adds functions that creates the Gs-Record for records
       that are not present in gs db table.

--- invenio_global_search/cli.py
+++ invenio_global_search/cli.py
@@ -10,32 +10,84 @@
 from warnings import warn
 
 from invenio_access.permissions import system_identity
+from invenio_drafts_resources.records import Record
+from invenio_records_global_search.records.api import GlobalSearchRecord
 
-from .components import map_metadata_from_a_to_b
+from .components import GlobalSearchSerializerType, map_metadata_from_a_to_b
 from .serializers import (
     LOMRecordJSONSerializer,
     Marc21RecordJSONSerializer,
     RDMRecordJSONSerializer,
 )
 
+
+def _base_rebuild_database_fn(
+    record_model_cls: type[Record],
+    serializer: GlobalSearchSerializerType,
+    schema: str,
+    metadata_cls: type | None = None,
+) -> None:
+    records = record_model_cls.model_cls.query.all()  # ty: ignore[unresolved-attribute]
+    for rec in records:
+        record = record_model_cls(rec.data, model=rec)
+        map_metadata_from_a_to_b(
+            record,
+            serializer_cls=serializer,
+            metadata_cls=metadata_cls,
+            schema=schema,
+            identity=system_identity,
+        )
+
+
+def _base_update_missing_fn(
+    record_model_cls: type[Record],
+    serializer: GlobalSearchSerializerType,
+    schema: str,
+    metadata_cls: type | None = None,
+) -> None:
+    gs_rdm_records_ids = []
+    all_gs_records = GlobalSearchRecord.model_cls.query.all()
+    for gs_rec in all_gs_records:
+        gs_record = GlobalSearchRecord(gs_rec.data, model=gs_rec)
+        if gs_record["original"]["schema"] == schema:
+            gs_rdm_records_ids.append(gs_record["original"]["pid"])
+
+    records = record_model_cls.model_cls.query.all()  # ty: ignore[unresolved-attribute]
+    for rec in records:
+        record = record_model_cls(rec.data, model=rec)
+        if (
+            record["id"] not in gs_rdm_records_ids
+            and record["access"]["record"] == "public"
+            and "tombstone" not in record
+        ):
+            map_metadata_from_a_to_b(
+                record,
+                serializer_cls=serializer,
+                metadata_cls=metadata_cls,
+                schema=schema,
+                identity=system_identity,
+            )
+
+
 try:
     from invenio_rdm_records.records.api import RDMRecord
 
     def rebuild_database_rdm() -> None:
         """Rebuild index rdm."""
-        records = RDMRecord.model_cls.query.all()
-        for rec in records:
-            record = RDMRecord(rec.data, model=rec)
-            map_metadata_from_a_to_b(
-                record,
-                serializer_cls=RDMRecordJSONSerializer,
-                schema="rdm",
-                identity=system_identity,
-            )
+        _base_rebuild_database_fn(RDMRecord, RDMRecordJSONSerializer, "rdm")
+
+    def update_missing_rdm() -> None:
+        """Update GS with missing rdm records."""
+        _base_update_missing_fn(RDMRecord, RDMRecordJSONSerializer, "rdm")
 
 except ImportError:
 
     def rebuild_database_rdm() -> None:
+        """Warn dummy function."""
+        msg = "The invenio-rdm-records package is not installed into your system."
+        warn(msg, stacklevel=2)
+
+    def update_missing_rdm() -> None:
         """Warn dummy function."""
         msg = "The invenio-rdm-records package is not installed into your system."
         warn(msg, stacklevel=2)
@@ -47,20 +99,30 @@ try:
 
     def rebuild_database_lom() -> None:
         """Rebuild index lom."""
-        records = LOMRecord.model_cls.query.all()
-        for rec in records:
-            record = LOMRecord(rec.data, model=rec)
-            map_metadata_from_a_to_b(
-                record,
-                serializer_cls=LOMRecordJSONSerializer,
-                metadata_cls=LOMMetadata,
-                schema="lom",
-                identity=system_identity,
-            )
+        _base_rebuild_database_fn(
+            LOMRecord,
+            LOMRecordJSONSerializer,
+            "lom",
+            metadata_cls=LOMMetadata,
+        )
+
+    def update_missing_lom() -> None:
+        """Update GS with missing lom records."""
+        _base_update_missing_fn(
+            LOMRecord,
+            LOMRecordJSONSerializer,
+            "lom",
+            metadata_cls=LOMMetadata,
+        )
 
 except ImportError:
 
     def rebuild_database_lom() -> None:
+        """Warn dummy function."""
+        msg = "The invenio-records-lom package is not installed into your system."
+        warn(msg, stacklevel=2)
+
+    def update_missing_lom() -> None:
         """Warn dummy function."""
         msg = "The invenio-records-lom package is not installed into your system."
         warn(msg, stacklevel=2)
@@ -72,20 +134,30 @@ try:
 
     def rebuild_database_marc21() -> None:
         """Rebuild index marc21."""
-        records = Marc21Record.model_cls.query.all()
-        for rec in records:
-            record = Marc21Record(rec.data, model=rec)
-            map_metadata_from_a_to_b(
-                record,
-                serializer_cls=Marc21RecordJSONSerializer,
-                metadata_cls=Marc21Metadata,
-                schema="marc21",
-                identity=system_identity,
-            )
+        _base_rebuild_database_fn(
+            Marc21Record,
+            Marc21RecordJSONSerializer,
+            "marc21",
+            metadata_cls=Marc21Metadata,
+        )
+
+    def update_missing_marc21() -> None:
+        """Update GS with missing marc21 records."""
+        _base_update_missing_fn(
+            Marc21Record,
+            Marc21RecordJSONSerializer,
+            "marc21",
+            metadata_cls=Marc21Metadata,
+        )
 
 except ImportError:
 
     def rebuild_database_marc21() -> None:
+        """Warn dummy function."""
+        msg = "The invenio-records-marc21 package is not installed into your system."
+        warn(msg, stacklevel=2)
+
+    def update_missing_marc21() -> None:
         """Warn dummy function."""
         msg = "The invenio-records-marc21 package is not installed into your system."
         warn(msg, stacklevel=2)
